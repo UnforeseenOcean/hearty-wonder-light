e24f025192b136b14ca964c920ceb22ac1bd425e
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2018-06-18 20:54:34 -0400

    call scan and deviceId on everything

--- shield.js
+++ shield.js
@@ -8,7 +8,10 @@ export const defaults= {
 	busOptions: null,
 	i2c: async function(){
 		return openI2c( this.busNumber, this.busOptions)
-	}
+	},
+	pcaAddress: 0x60,
+	mcpAddress: 0x40,
+	tmpAddress: 0x4C
 }
 
 function promisize( fns, o){
@@ -20,7 +23,21 @@ function promisize( fns, o){
 }
 const promisizeI2c= promisize.bind(undefined, ["i2cFuncs", "scan", "deviceId", "i2cRead", "i2cWrite"])
 
-export class Shield{
+async function validateI2c( i2c){
+	const funs= await i2c.i2cFuncs()
+	if( !funs.i2c){
+		throw new Error( "Unexpected non-i2c bus")
+	}
+	return i2c
+}
+
+function err(msg, o){
+	const err= new Error( msg)
+	Object.assign( err, o)
+	return err
+}
+
+export class HPLedShield{
 	static get defaults(){
 		return defaults
 	}
@@ -31,18 +48,43 @@ export class Shield{
 	constructor( opts){
 		const _defaults= opts&& opts.defaults!== undefined? opts.defaults|| defaults
 		const opts= Object.assign( {}, _defaults, opts)
+		// warning: we will concretize this i2c member when it resolves!!
 		this.i2c= i2c.call( opts) // get bus
 		  .then( promisizeI2c) // promisify
+		  .then( validateI2c) // this isn't a smbus right?
+		  .then( this.loadDevices.bind(this))
 		  .then( i2c=> this.i2c= i2c) // bus is now ready; concretize assignment
 	}
 
-	// methods assume bus is ready.
-
-	verify(){
-		
+	async loadDevices(){
+		await verify()
+		const devices= ["pca", "mcp", "tmp"].map(device=> {
+			// scan it down first
+			const
+			  addr= `${device}Address`,
+			  [device]= await this.i2c.scan( addr, addr)
+			if( !device){
+				throw err(`could not scan device ${device}`, {device})
+			}
+			// retrieve info
+			const id= `${device}Id`,
+			this[ id]= await this.i2c.deviceId( address),
+		})
+		return Promise.all( devices).then(()=> return this)
 	}
+	async verify(){
+		// have we a bus?
+		await validateI2c( this.i2c)
+		// have all devices report to us
+		// effect: re-sets our device id's.
+		await this.loadDevice()
+		return this
+	}
+
+	// methods assume bus is ready.
 
 	begin(){
 		
 	}
 }
+export default HPLedShield
